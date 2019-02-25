f5c857b0191893a7220fffda90d54e5106221cf8
rbanh <rbanh@me.com>
2019-02-24 23:41:26 -0800

    iono wtf is wrong

--- sprint2/src/App.js
+++ sprint2/src/App.js
@@ -6,6 +6,7 @@ import { Route, Switch} from 'react-router-dom'
 import axios from 'axios'
 const dakey= '?api_key=58d3de8d-b26f-49c9-bb56-b810f7c8432e'
 const urlvid = `https://project-2-api.herokuapp.com/videos${dakey}`
+const currentvid = id => `https://project-2-api.herokuapp.com/videos${id}${dakey}`
 
 
 class App extends Component {
@@ -14,35 +15,30 @@ class App extends Component {
     super();
     this.state = 
     {
-      data: null,
-      id:'',
-      comment:[]
+      datas: null,
+      ids:'',
+      current:null
     };
   }
 
   componentDidMount(){
-   
     axios
-    .get(urlvid)
-      .then(response=>{
-        this.setState({data:response.data, id:response.data[0].id})
+    .get( urlvid )
+      .then( response => {
+        this.setState({datas: response.data})
+        this.setState({ids: response.data.id })
       })
-      const currentvideo = `https://project-2-api.herokuapp.com/videos/${this.state.id}${dakey}`
       .then(()=>{
-        axios
-          .get(currentvideo)
-          .then(response=>{
-            this.setState({
-              comment:response.data.comments
-            })
+        axios.get( currentvid(this.state.ids) )
+          .then( response => {
+            console.log(response.data)
+            this.setState({current: response.data})
           })
-
       })
   }
  
   render() {
-    const suggestedlistdata = this.state.data
-    console.log(suggestedlistdata)
+    const suggestedlistdata = this.state.datas
     const videosection = () =><Videosection mainVideo={this.props.mainVideo} sideVideo={suggestedlistdata}/>
     const up = () => <Videoup mainVideo={this.props.mainVideo}/>
     return (
