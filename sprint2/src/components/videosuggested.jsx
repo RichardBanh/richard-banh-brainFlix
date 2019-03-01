8e175b6c076a9d81ba6ad021e4ade618dd809e44
rbanh <rbanh@me.com>
2019-03-01 11:28:05 -0800

    formated

--- sprint2/src/components/videosuggested.jsx
+++ sprint2/src/components/videosuggested.jsx
@@ -7,26 +7,30 @@ class Videosug extends Component {
     if (this.props.suggestedlistdata === null) {
       return <div>Loading</div>;
     } else {
-      var suggestedlistdata = this.props.suggestedlistdata.filter((boop)=>{return ( boop.id!== this.props.mainVideo.id)}).map(stoop => {
-        return (
-          <div>
-            <Link
-              onClick={() =>
-                this.props.axioscall(this.props.urlonclick(stoop.id))
-              }
-              to={"/" + stoop.id}
-            >
-              <div className="titlesimg">
-                <img src={stoop.image} alt="" />
-                <div className="titles">
-                  <h4>{stoop.title}</h4>
-                  <h5>{stoop.channel}</h5>
+      var suggestedlistdata = this.props.suggestedlistdata
+        .filter(boop => {
+          return boop.id !== this.props.mainVideo.id;
+        })
+        .map(stoop => {
+          return (
+            <div>
+              <Link
+                onClick={() =>
+                  this.props.axioscall(this.props.urlonclick(stoop.id))
+                }
+                to={"/" + stoop.id}
+              >
+                <div className="titlesimg">
+                  <img src={stoop.image} alt="" />
+                  <div className="titles">
+                    <h4>{stoop.title}</h4>
+                    <h5>{stoop.channel}</h5>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        );
-      });
+              </Link>
+            </div>
+          );
+        });
     }
     return suggestedlistdata;
   }
